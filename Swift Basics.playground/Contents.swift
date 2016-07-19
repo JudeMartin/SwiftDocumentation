9f35f7659e8762106e5e9fe132bdb4b5ef9cf0a8
Jude Martin <judemartin.mj@gmail.com>
2016-07-19 02:16:03 -0700

    Using a Dictionary

--- Swift Basics.playground/Contents.swift
+++ Swift Basics.playground/Contents.swift
@@ -127,3 +127,25 @@ repeat {
     latestIndex++
 }
 while latestIndex < 3
+
+
+/* labelled Loop */
+let interestingNumbers = [
+    "Prime" : [2,3,5,7,11,13],
+    "Fibonacci" : [1,1,2,3,5,8],
+    "Square" : [1,4,9,16,25]
+]
+var largest = 0
+for(kind, numbers) in interestingNumbers {
+    for number in numbers {
+        if number > largest {
+            largest = number
+        }
+    }
+}
+print("The largest number is \(largest) in the Dictionary" )
+var total = 0
+for i in 0...4 {
+    total += i
+}
+print(total)
